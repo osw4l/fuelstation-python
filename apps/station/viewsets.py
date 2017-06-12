081ac7cb5b0e6f56c8285c1da31b72b33b1134b8
0sw4l <ioswxd@gmail.com>
2017-06-11 21:09:01 -0500

    set viewsets to name list

--- apps/station/viewsets.py
+++ apps/station/viewsets.py
@@ -8,6 +8,16 @@ from . import models
 class EstacioViewSet(viewsets.ModelViewSet):
     queryset = models.Estacion.objects.all()
     serializer_class = serializers.EstacionSerializer
+    model = models.Estacion
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('id')
+
+    def list(self, request, *args, **kwargs):
+    	serializer = self.serializer_class(self.get_queryset(), many=True)
+    	return Response({
+            'estaciones': serializer.data
+        })
 
     @detail_route(methods=['get'])
     def combustibles(self, request, pk=None):
